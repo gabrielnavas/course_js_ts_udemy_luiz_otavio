6860b3d67ac8593f8db207d76a89ff0324c744f9
Gabriel Navas <gabrielnavas@email.com>
2020-08-08 09:56:33 -0300

    console.log

---
+++ javascript_basico/consolelog.js
@@ -0,0 +1,10 @@
+const numeroInteiro = 22; //number
+const numeroFlutuante = 22.66; //number
+const sequenciaCaracteres = 'Gabriel foi pescar'; //string
+
+console.log('número inteiro: ', numeroInteiro);
+console.log('número flutuante: ', numeroFlutuante);
+console.log('sequência de caracteres (string): ', sequenciaCaracteres);
+
+
+console.log('mostra tudo: ', numeroInteiro, numeroFlutuante, sequenciaCaracteres);
