16430b2924ed9a193ac54e72b579c1446cd458e1
Gabriel Navas <gabrielnavas@email.com>
2020-08-12 20:50:50 -0300

    exercicio, tabela IMC gabriel navas

---
+++ javascript_logica_programacao/tabela_imc_gabriel/assets/js/main.js
@@ -0,0 +1,120 @@
+(function escopoCalcularIMC() {
+
+    const calcularIMC = (peso, altura) => peso / (altura * altura);
+
+    function obterIMC() {
+        const elemPeso = document.querySelector('#peso');
+        const elemAltura = document.querySelector('#altura');
+
+        const peso = Number(elemPeso.value);
+        const altura = Number(elemAltura.value);
+
+        const resultadoIMC = calcularIMC(peso, altura);
+        return resultadoIMC;
+    }
+
+    function escolherRespostaClassCSS(imc) {
+        let classImc = '#imc_op';
+        let classResultado = '#resul_op';
+
+        if (imc < 18.5) {
+            classImc += '1';
+            classResultado += '1';
+        }
+        else if (imc >= 18.5 && imc < 25) {
+            classImc += '2';
+            classResultado += '2';
+        }
+        else if (imc >= 25.5 && imc < 30) {
+            classImc += '3';
+            classResultado += '3';
+        }
+        else if (imc >= 30.5 && imc < 35) {
+            classImc += '4';
+            classResultado += '4';
+        }
+        else if (imc >= 35.5 && imc < 40) {
+            classImc += '5';
+            classResultado += '5';
+        }
+        else {
+            classImc += '6';
+            classResultado += '6';
+        }
+
+        return {
+            classImc,
+            classResultado
+        }
+    }
+
+
+    function mostrarNoHtmlResultados({ classImc, classResultado }) {
+
+        const elemLiImc = document.querySelector(classImc);
+        const elemLiResult = document.querySelector(classResultado);
+
+        console.log(elemLiImc);
+        console.log(elemLiResult);
+
+        function setCssElementosLi({ elemento, innerHTML, fontWeight, color }) {
+            elemento.innerHTML = innerHTML;
+            elemento.style.fontWeight = fontWeight;
+            elemento.style.color = color;
+        }
+
+        function limparResultadoAnterior() {
+            
+            for(let numero=1; numero <= 6; numero++) {
+
+                const elemImcClear = document.querySelector(`#imc_op${numero}`);
+                const elemRespClear = document.querySelector(`#resul_op${numero}`);
+                
+                setCssElementosLi({
+                    elemento: elemImcClear,
+                    innerHTML: elemImcClear.innerHTML.replace('-&gt; ', ''),
+                    fontWeight: 'initial',
+                    textColor: 'initial',
+                });
+                setCssElementosLi({
+                    elemento: elemRespClear,
+                    innerHTML: elemRespClear.innerHTML.replace('-&gt; ', ''),
+                    fontWeight: 'initial',
+                    textColor: 'initial',
+                });
+            }
+        }
+
+        function novoResultado() {
+            setCssElementosLi({
+                elemento: elemLiImc,
+                innerHTML: `-> ${elemLiImc.innerHTML}`,
+                fontWeight: 'bold',
+                textColor: 'darkblue',
+            });
+            setCssElementosLi({
+                elemento: elemLiResult,
+                innerHTML: `-> ${elemLiResult.innerHTML}`,
+                fontWeight: 'bold',
+                textColor: 'darkblue',
+            });
+        }
+
+        limparResultadoAnterior();
+        novoResultado();
+    }
+
+    function listenerEventCalcularIMC(event) {
+        event.preventDefault();
+
+        const imc = obterIMC();
+        const respClassesCSS = escolherRespostaClassCSS(imc);
+
+        mostrarNoHtmlResultados(respClassesCSS);
+    }
+
+    (function main() {
+        const btnEnviar = document.querySelector('#form');
+        btnEnviar.addEventListener('submit', listenerEventCalcularIMC);
+    })();
+})();
