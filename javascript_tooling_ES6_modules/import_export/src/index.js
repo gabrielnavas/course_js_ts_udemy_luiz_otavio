083bb292a6bb3d8ce73ffd314eecdf11d92a4f1e
Gabriel Navas <gabrielnavas@email.com>
2020-08-25 18:51:15 -0300

    import e export

---
+++ javascript_tooling_ES6_modules/import_export/src/index.js
@@ -0,0 +1,6 @@
+import modulo1, { soma as SomaFunc } from './modulo1';
+
+
+const modulo = new modulo1();
+const soma = SomaFunc(1, 2);
+console.log(result);
---
+++ javascript_tooling_ES6_modules/import_export/src/modulo1.js
@@ -0,0 +1,13 @@
+export default class {
+    constructor() {
+        console.log('foi construido');
+    }
+}
+
+function soma(x, y) {
+    return x + y;
+}
+
+export {
+    soma,   
+};
