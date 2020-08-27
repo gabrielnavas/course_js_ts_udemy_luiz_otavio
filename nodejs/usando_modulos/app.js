84ba7384e98ebb41a5bfccefe6241d2071172329
Gabriel Navas <gabrielnavas@email.com>
2020-08-26 21:16:28 -0300

    modulos (2) node

--- nodejs/usando_modulos/app.js
+++ nodejs/usando_modulos/app.js
@@ -1,5 +1,5 @@
 // const mod1 = require('./mod1');
-const { falaNome, nome, MinhaClasse } = require('./mod1');
+// const { falaNome, nome, MinhaClasse } = require('./mod1');
 // require('./usandoLibsExistentes');
 
 // console.log(mod1);
@@ -11,6 +11,16 @@ Luiz Miranda
 */
 
 
-console.log(nome, falaNome()); //Luiz Luiz Miranda
+// console.log(nome, falaNome()); //Luiz Luiz Miranda
 
-new MinhaClasse(); // minha classe anonima foi instânciada
+// new MinhaClasse(); // minha classe anonima foi instânciada
+
+// const soma = require('./mod2');
+// soma(2,4);
+
+const path = require('path');
+
+console.log(path.resolve(__dirname, '..', '..')); // /home/navas/Desktop/course_js_ts_udemy_luiz_otavio
+
+console.log(__filename); // /home/navas/Desktop/course_js_ts_udemy_luiz_otavio/nodejs/usando_modulos/app.js
+console.log(__dirname); // /home/navas/Desktop/course_js_ts_udemy_luiz_otavio/nodejs/usando_modulos
---
+++ nodejs/usando_modulos/mod2.js
@@ -0,0 +1,8 @@
+// export default example
+
+// module.exports = function (x, y) {
+//     console.log(x+y);
+// }
+
+// module.exports(2, 3);
+
