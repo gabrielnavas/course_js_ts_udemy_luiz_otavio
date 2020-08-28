b068b28374e48c2bb2ff3ef61899100b00aa6bae
Gabriel Navas <gabrielnavas@email.com>
2020-08-28 02:18:34 -0300

    express introducao

---
+++ nodejs/express_introducao/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form method="POST" action="/">
+            nome:
+            <input type="text" name="nome">
+            <button>ir</button>
+        </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('recebi o formulario.');
+})
+
+
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+    console.log('Executando na porta 3000');
+});
