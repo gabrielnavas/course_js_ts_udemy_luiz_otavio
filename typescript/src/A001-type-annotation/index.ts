cdf53b1bc80d25a479eb0ed9ca7032adaafc2042
navas <gabrielnavas@email.com>
2020-09-15 17:05:00 -0300

    SOLID projeto configs

---
+++ typescript/src/A001-type-annotation/index.ts
@@ -0,0 +1,7 @@
+let nome: string = 'Gabriel';
+let idade: number = 30;
+let peso: number = 75.8;
+let cor = 0xf00d;
+let adulto: boolean = true;
+let simbol: symbol = Symbol('☺');
+// let big: bigint = 10n;
